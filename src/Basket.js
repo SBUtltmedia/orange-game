8b1660b5797e509f3b342d294007f40b39f591d3
Ken Fehling <ken@androidideas.org>
2015-06-18 15:00:37 -0400

    Derek & Paul

---
+++ src/Basket.js
@@ -0,0 +1,19 @@
+import React, { PropTypes, Component } from 'react';
+import DropArea from './DropArea';
+import ItemTypes from './ItemTypes';
+
+export default class Basket{
+
+  render() {
+
+    return (
+<DropArea
+
+		  accepts={[ItemTypes.ORANGE]}
+              onDrop={(item) => this.handleDrop()}
+>
+{this.props.children}
+</DropArea>
+    );
+  }
+}
