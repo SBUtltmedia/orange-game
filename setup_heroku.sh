2a5065b2666c34ce61fbcc9e9cdc886e38d1b788
Ken Fehling <ken@androidideas.org>
2015-06-18 19:44:53 -0400

    Trying to fix gulp build on Heroku

--- setup_heroku.sh
+++ setup_heroku.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 APP="orange-game"
 
-heroku config:add BUILDPACK_URL=https://github.com/appstack/heroku-buildpack-nodejs-gulp.git --app $APP;
+heroku config:set BUILDPACK_URL=https://github.com/krry/heroku-buildpack-nodejs-gulp-bower.git --app $APP;
+heroku config:set NPM_CONFIG_PRODUCTION=false --app $APP;
 heroku config:set NODE_ENV=production --app $APP;
