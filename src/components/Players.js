6ceb2eff0770a94a6892a4247f24677328e23504
Kenneth T Fehling <kfehling@dh017-169.medilab.tlt.stonybrook.edu>
2015-07-06 18:52:05 -0400

    re-base seems kinda flaky

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,12 +1,13 @@
 import React, { PropTypes, Component } from 'react/addons';
 import { areaTheme } from '../styles/Themes';
 import Player from './Player';
+import _ from 'lodash';
 
 import Rebase from 're-base';
 import { FIREBASE_APP_URL } from '../constants/Settings';
 const base = Rebase.createClass(FIREBASE_APP_URL + '/games');
 
-var userId = 277;
+var userId = Math.ceil(Math.random() * 99999999999999);
 var amOnline = new Firebase(`${FIREBASE_APP_URL}/.info/connected`);
 var userRef = new Firebase(`${FIREBASE_APP_URL}/presence/${userId}`);
 
@@ -25,7 +26,8 @@ export default class Players extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            players: []
+            players: [],
+            me: { name: '' + userId }
         };
     }
 
@@ -40,15 +42,29 @@ export default class Players extends Component {
             state: 'players',
             asArray: true
         });
-        amOnline.on('value', function(snapshot) {
-            if (snapshot.val()) {
-                userRef.onDisconnect().remove();
+        amOnline.on('value', function(online) {
+            if (online.val()) {
+                //userRef.onDisconnect().remove();
                 userRef.set(true);
 
-                const player = { name: 'Player ' + Math.ceil(Math.random() * 100)};
+                const player = { name: '' + userId, online: true };
                 this.setState({
                     players: this.state.players.concat([player])
                 });
+            }
+            else {
+                const players = this.state.players;
+
+                console.log(players);
+
+                const me = _.findWhere(players, { name: this.state.me.name });
+
+                if (me) {
+                    me.online = false;
+                    this.setState({
+                        players: players
+                    });
+                }
             }
         }.bind(this));
     }
@@ -59,6 +75,9 @@ export default class Players extends Component {
 
     render() {
         const { players } = this.state;
+
+        console.log(players);
+
         return <div style={styles.container}>
             { players.map((p, i) => <Player key={i} name={p.name} />) }
         </div>
