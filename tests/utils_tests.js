a5bb1f550020069788574acdb6b9265b095c23ef
Ken Fehling <ken@androidideas.org>
2015-07-14 16:06:05 -0400

    Updated redux and cleaned up package.son

--- tests/utils_tests.js
+++ tests/utils_tests.js
@@ -3,10 +3,10 @@ var chai = require('chai');
 var expect = chai.expect;
 
 describe('utils', function() {
-  describe('objectToArray', function() {
-    it('should convert an object to an array', function() {
-        var obj = { id123: { val: 1 }};
-        expect(utils.objectToArray(obj)).to.deep.equal([{ id: 'id123', val: 1 }]);
-    });
+    describe('objectToArray', function() {
+        it('should convert an object to an array', function() {
+            var obj = { id123: { val: 1 }};
+            expect(utils.objectToArray(obj)).to.deep.equal([{ id: 'id123', val: 1 }]);
+        });
     });
 });
