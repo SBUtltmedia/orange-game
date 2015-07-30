31b373d36acede5fa85ca4c5cc9296f99cae1d71
Ken Fehling <ken@androidideas.org>
2015-07-30 14:06:34 -0400

    Market still needs a lot of work

--- src/components/Market.js
+++ src/components/Market.js
@@ -1,6 +1,9 @@
 import React, { PropTypes, Component } from 'react';
 import Modal from 'react-modal';
 import { APP_ROOT_ELEMENT } from '../constants/Settings';
+import { subscribeToFirebaseList, getFbRef } from '../utils';
+import _ from 'lodash';
+import model from '../model';
 
 const appElement = document.getElementById(APP_ROOT_ELEMENT);
 Modal.setAppElement(appElement);
@@ -14,8 +17,18 @@ export default class Market extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            modalIsOpen: props.open
+            modalIsOpen: props.open,
+            players: []
         };
+    }
+
+    componentWillMount() {
+        this.firebaseRef = getFbRef(`/games/${model.gameId}/players`);
+        subscribeToFirebaseList(this, this.firebaseRef, 'players', 'authId');
+    }
+
+    componentWillUnmount() {
+        this.firebaseRef.off();
     }
 
     closeModal() {
@@ -27,18 +40,28 @@ export default class Market extends Component {
     }
 
     render() {
-        return <Modal isOpen={this.state.modalIsOpen}
-                      onRequestClose={() => this.closeModal()}>
-
-          { /* Close button or 'X' */ }
-
+        const { modalIsOpen, players } = this.state;
+        return <Modal isOpen={modalIsOpen} onRequestClose={() => this.closeModal()}>
           <h2>Market</h2>
               <table>
-                <tr>
-                    <td>
-
-                    </td>
-                </tr>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Fitness</td>
+                        <td>Available Oranges</td>
+                        <td>Debt</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        _.map(players, p => <tr>
+                            <td>{p.name}</td>
+                            <td>{p.fitness}</td>
+                            <td>{p.oranges.box + p.oranges.basket}</td>
+                            <td>?</td>
+                        </tr>)
+                    }
+                </tbody>
             </table>
             <button onClick={() => this.closeModal()}>Close</button>
         </Modal>;
