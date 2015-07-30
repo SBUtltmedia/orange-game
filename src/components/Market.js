6a8d2ed8fbc7c6f98c4f49637b7c8c1f6d815d69
Ken Fehling <ken@androidideas.org>
2015-07-29 20:22:56 -0400

    Started Market modal

---
+++ src/components/Market.js
@@ -0,0 +1,38 @@
+import React, { PropTypes, Component } from 'react';
+import Modal from 'react-modal';
+import { APP_ROOT_ELEMENT } from '../constants/Settings';
+
+const appElement = document.getElementById(APP_ROOT_ELEMENT);
+Modal.setAppElement(appElement);
+Modal.injectCSS();
+
+export default class Market extends Component {
+    static propTypes = {
+        open: PropTypes.bool.isRequired
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            modalIsOpen: props.open
+        };
+    }
+
+    closeModal() {
+        this.setState({ modalIsOpen: false });
+    }
+
+    render() {
+        return <Modal isOpen={this.state.modalIsOpen}
+                      onRequestClose={() => this.closeModal()}>
+          <h2>Market</h2>
+              <table>
+                <tr>
+                    <td>
+
+                    </td>
+                </tr>
+            </table>
+        </Modal>;
+    }
+}
