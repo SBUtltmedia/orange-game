11b853104bf9ecd115d82ad33dda5c7e86d629d8
Ken Fehling <kenneth.fehling@stonybrook.edu>
2015-08-12 19:48:47 -0400

    Trying 0.0.0.0

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 var port = process.env.PORT || 3000;
+var host = '0.0.0.0';
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(port, 'localhost', function (err, result) {
+}).listen(port, host, function (err, result) {
   if (err) {
     console.log(err);
   }
 
-  console.log('Listening at localhost:' + port);
+  console.log('Listening on port ' + port);
 });
