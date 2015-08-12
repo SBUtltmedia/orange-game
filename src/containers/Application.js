11a1657f7d658fce2d8eb7cfa469f7f58f0ea700
Ken Fehling <kenneth.fehling@stonybrook.edu>
2015-08-12 15:32:34 -0400

    Good start, now need to do images

--- src/containers/Application.js
+++ src/containers/Application.js
@@ -1,17 +1,10 @@
 import React, { Component } from "react";
-import StyleSheet from'react-style';
 import { RouteHandler } from 'react-router';
 import Menu from '../components/Menu';
 
-const styles = StyleSheet.create({
-    page: {
-        backgroundColor: '#ffad00'
-    }
-});
-
 export default class Application extends Component {
     render() {
-        return <div style={styles.page}>
+        return <div>
             <Menu />
             <RouteHandler {...this.props} />
         </div>
