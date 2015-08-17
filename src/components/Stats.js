99bd9e6c6b941487b5aaaf27a2ff9eb5c22ae93c
Ken Fehling <kenneth.fehling@stonybrook.edu>
2015-08-17 18:44:10 -0400

    implemented Stats in the new way

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,7 +1,7 @@
 import React, { PropTypes, Component } from 'react';
 import { areaTheme, verticalCenter } from '../styles/Themes';
 import model from '../model';
-import { subscribeToFbObject, getFbRef } from '../utils';
+import { connect } from 'redux/react';
 
 const styles = {
   container: {
@@ -39,63 +39,52 @@ function formatChange(change) {
     }
 }
 
+@connect(state => ({
+    firebase: state.firebase
+}))
 export default class Stats extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            stats: {
-                day: null,
-                fitness: null,
-                fitnessChange: null
-            }
-        };
-    }
-
-    componentWillMount() {
-        const { gameId, authId } = model;
-        const url = `/games/${gameId}/players/${authId}`;
-        this.firebaseRef = getFbRef(url);
-        subscribeToFbObject(this, this.firebaseRef, 'stats');
-    }
-
-    componentWillUnmount() {
-        this.firebaseRef.off();
-    }
-
     render() {
-        const { day, fitness, fitnessChange } = this.state.stats;
-        var fitnessChangeColor = getFitnessChangeColor(fitnessChange);
-
-        return <div style={styles.container}>
-            <br />
-            <h4>Your stats</h4>
-            <br /><br />
-            <div style={styles.inner}>
-                <p>
-                    <span>Day:</span>
-                    &nbsp;
-                    <span style={styles.value}>{day}</span>
-                </p>
-                <p>
-                    <span>Fitness:</span>
-                    &nbsp;
-                    <span style={styles.value}>{fitness}</span>
-                </p>
-                <p>
-                    <span>Change:</span>
-                    &nbsp;
-                    <span style={{...styles.value, color: fitnessChangeColor}}>
-                                    {formatChange(fitnessChange)}
-                    </span>
-                </p>
-                <p>
-                    <span>Loans:</span>
-                    <ul>
-                        <li>John: <span style={styles.value}>3</span></li>
-                    </ul>
-                </p>
-            </div>
-        </div>;
+        const { firebase } = this.props;
+        if (firebase) {
+            const { games } = firebase;
+            if (games) {
+                const game = games[model.gameId];
+                const player = game.players[model.authId];
+                const { fitness, fitnessChange, day } = player;
+                var fitnessChangeColor = getFitnessChangeColor(fitnessChange);
+                return <div style={styles.container}>
+                    <br />
+                    <h4>Your stats</h4>
+                    <br /><br />
+                    <div style={styles.inner}>
+                        <p>
+                            <span>Day:</span>
+                            &nbsp;
+                            <span style={styles.value}>{day}</span>
+                        </p>
+                        <p>
+                            <span>Fitness:</span>
+                            &nbsp;
+                            <span style={styles.value}>{fitness}</span>
+                        </p>
+                        <p>
+                            <span>Change:</span>
+                            &nbsp;
+                            <span style={{...styles.value, color: fitnessChangeColor}}>
+                                            {formatChange(fitnessChange)}
+                            </span>
+                        </p>
+                        <p>
+                            <span>Loans:</span>
+                            <ul>
+                                <li>John: <span style={styles.value}>3</span></li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>;
+            }
+        }
+        return <div style={styles.container}></div>  // fallback
     }
 }
