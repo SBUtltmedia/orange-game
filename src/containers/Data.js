b3a27cbf1afb2a339b98841811e39a0f767a7559
Ken Fehling <kenneth.fehling@stonybrook.edu>
2015-11-23 17:36:02 -0500

    Getting further with simple CSV download

--- src/containers/Data.js
+++ src/containers/Data.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import StyleSheet from'react-style';
 import { Link } from 'react-router';
+import { connect } from 'redux/react';
+import * as FluxActions from '../actions/FluxActions';
+import { bindActionCreators } from 'redux';
+import _ from 'lodash';
+import { getAllGames } from '../gameUtils';
 
 const styles = StyleSheet.create({
     page: {
@@ -9,13 +14,36 @@ const styles = StyleSheet.create({
     }
 });
 
+function getGamesData(firebase) {
+    const games = getAllGames(firebase);
+    console.log(games);
+    return _.map(games, g => `${'A'},${_.size(g.players)}`).join('\n');
+}
+
+function getData(firebase) {
+    return 'Game,Players\n' + getGamesData(firebase);
+}
+
+@connect(state => ({
+    firebase: state.firebase
+}))
 export default class Data extends Component {
+    componentWillMount() {
+        const { dispatch, firebase } = this.props;
+        this.fluxActions = bindActionCreators(FluxActions, dispatch);
+        this.fluxActions.listenToFirebase();
+    }
+
+    componentWillUnmount() {
+        this.fluxActions.disconnectFromFirebase();
+    }
 
     render() {
-        const data = '1,2,3';
+        const { firebase } = this.props;
+        const data = getData(firebase);
         return <div styles={[styles.page]}>
             <a download="orange-game_data.csv"
-                href={"data:text/csv;charset=utf-8,"+ data}>Download</a>
+                href={"data:text/csv;charset=utf-8,"+ encodeURI(data)}>Download</a>
         </div>;
     }
 }
